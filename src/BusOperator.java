00f0613cc33224b093d593eebdcbb3b7bd8f858a
quaktuss <mathieupheron1@gmail.com>
2021-10-26 11:41:57 +0200

    add

--- src/BusOperator.java
+++ src/BusOperator.java
@@ -1,9 +1,10 @@
 public class BusOperator extends Human {
-    private int license;
+    public int license;
 
     public BusOperator(String name, int age, String sex, int license) {
         this.name = name;
-        if (age >= 18) this.age = age; else {
+        if (age >= 18) this.age = age;
+        else {
             System.out.println("You cant be a bus operator.");
             return;
         }
